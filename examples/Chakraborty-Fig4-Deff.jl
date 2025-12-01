5c0d9eecf78a9378876b292f0048d75f68b4b41a
Jarvist Moore Frost <jarvist@gmail.com>
2025-12-01 11:41:32 +0000

    Insufficient beads in the path integral ^_^ - sort of working now T=100

--- examples/Chakraborty-Fig4-Deff.jl
+++ examples/Chakraborty-Fig4-Deff.jl
@@ -1,16 +1,24 @@
 using IonMobility
 using Gnuplot
 
-chi_values = collect(0.01:0.05:1.5)
-D_ratios = [minimal_chakraborty_scpt(chi) for chi in chi_values]
+# function minimal_chakraborty_scpt(
+#   λκ::Float64;
+#   N::Int = 10,
+#   T::Float64 = 10.0,
+#   maxiter::Int = 200, )
 
-@gp chi_values D_ratios "w l t 'D_{eff}/D'"
+@gp "set title 'Effective Diffusivity vs Disorder Strength'"
 @gp :- "set xlabel 'λ_D * κ'"
 @gp :- "set ylabel 'D_{eff}/D'"
-@gp :- "set title 'Effective Diffusivity vs Disorder Strength'"
 @gp :- "set grid"
 @gp :- "set key top right"
 
+chi_values = collect(0.01:0.05:3.0)
+
+for T in [10.0,25.0,50.0,100.0]
+    D_ratios = [minimal_chakraborty_scpt(chi, T=T) for chi in chi_values]
+    @gp :- chi_values D_ratios "w lp t 'T: $T D_{eff}/D'"
+end
 
 Gnuplot.save("Chakraborty-Fig4-Deff.png", term="pngcairo size 800,600 enhanced font 'Helvetica,14'")
 println("Plot saved to Chakraborty-Fig4-Deff.png")
